f5438f3eb3b1c129e9f8530aa1f44aac6d8238b9
asano1413 <f7pati.nsa20@gmail.com>
2025-03-27 17:19:48 +0900

    fix and installvHerd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
-// import laravel from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+      laravel([
+          'resources/css/app.css',
+          'resources/js/app.js',
+      ]),
       tailwindcss(),
     ],
 });
